aeae49c08b2fb06f6aa25b71458b1545016f0272
ugendar143 <146059449+ugendar143@users.noreply.github.com>
2023-10-30 12:34:17 +0530

    Add files via upload

---
+++ LargeNumber.java
@@ -0,0 +1,19 @@
+package com.unocarrier;
+
+
+public class LargeNumber {
+
+	public static void main(String[] args) {
+
+		int num[]= {-7,0,8,19,20,15,-10,-60,8,-8};
+		int big=num[0];
+		for(int i=0;i<num.length;i++) {
+			if(big<num[i]) {
+				big=num[i];
+			}
+		}
+			System.out.println("largest number:"+big);
+	}
+	}
+
+
