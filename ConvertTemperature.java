4b64486904eabc1068ad0474afb185ee879e1b45
ugendar143 <146059449+ugendar143@users.noreply.github.com>
2023-11-18 14:42:29 +0530

    Add files via upload

---
+++ ConvertTemperature.java
@@ -0,0 +1,15 @@
+package uno.career;
+
+public class ConvertTemperature {
+
+	public static void main(String[] args) {
+	
+		float celsius;
+		System.out.println("\n conversion of fahrenheit to celsius");
+		float fahrenheit =50;
+		celsius=(fahrenheit-32)*5/9;
+		System.out.println("temperature in celsius:"+celsius);
+		
+	}
+
+}
