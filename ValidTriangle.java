12f502a8536ff836937d4eee3edb0433fd6fedb7
ugendar143 <146059449+ugendar143@users.noreply.github.com>
2023-11-18 14:46:52 +0530

    Add files via upload

---
+++ ValidTriangle.java
@@ -0,0 +1,18 @@
+package uno.career;
+
+public class ValidTriangle {
+
+	public static void main(String[] args) {
+		int t=180,total;
+		int s1=20;
+		int s2=60;
+		int s3=100;
+		total=s1+s2+s3;
+			if(total==180) {
+				System.out.println("Triangle is valid");
+		}else {
+			System.out.println("Triangle is not valid");
+		}
+	}
+
+}
