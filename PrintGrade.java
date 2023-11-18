ff71f8d19f78a312cb2ac794b911522e7298d39e
ugendar143 <146059449+ugendar143@users.noreply.github.com>
2023-11-18 14:43:20 +0530

    Add files via upload

---
+++ PrintGrade.java
@@ -0,0 +1,22 @@
+package uno.career;
+
+public class PrintGrade {
+
+	public static void main(String[] args) {
+		int a=55;
+		if(a<60) {
+			System.out.println("F");	
+		}
+		if(a>=60&&a<=74) {
+			System.out.println("C");
+		}
+		if(a>=75&&a<=89) {
+			System.out.println("B");
+		}
+		if(a>=90&&a<=100) {
+			System.out.println("A");
+		}
+	}
+	
+
+}
