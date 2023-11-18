09db3724f71af627b1d0c2076d7a74de00cbcfdc
ugendar143 <146059449+ugendar143@users.noreply.github.com>
2023-11-18 14:43:37 +0530

    Add files via upload

---
+++ PrintPattern.java
@@ -0,0 +1,15 @@
+package uno.career;
+
+public class PrintPattern {
+
+	public static void main(String[] args) {
+			for(int r=1;r<=5;r++) {
+			for(int c=1;c<=r;c++) {
+            System.out.print("*");
+			}
+			System.out.println( );
+		}
+					
+	}
+
+}
